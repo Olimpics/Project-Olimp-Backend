f4d703300c230ad7a5abe60197ba97597b8f0e9e
Anstrofon <tonzamko@yahoo.com>
2026-03-12 23:16:29 +0200

    Creating basicRepository

---
+++ Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+public interface IRepository<T> where T : class
+{
+    IQueryable<T> Query();
+
+    Task<T?> GetByIdAsync(int id);
+
+    Task AddAsync(T entity);
+
+    void Update(T entity);
+
+    void Remove(T entity);
+
+    Task SaveAsync();
+}
