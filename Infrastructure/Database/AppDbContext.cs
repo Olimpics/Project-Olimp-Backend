996a17c31dd00ee36731735c29737d5e76eb245d
Andrey Ruskykh <akvaforce@gmail.com>
2026-04-04 01:09:47 +0300

    Fix for server 9

--- Infrastructure/Database/AppDbContext.cs
+++ Infrastructure/Database/AppDbContext.cs
@@ -87,7 +87,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("host=127.0.0.1;port=3307;database=DNUProjectDb;username=user_dnupr;password=B25824DCABCB88B5", Microsoft.EntityFrameworkCore.ServerVersion.Parse("11.8.3-mariadb"));
+        => optionsBuilder.UseMySql("host=127.0.0.1;port=3306;database=DNUProjectDb;username=user_dnupr;password=B25824DCABCB88B5", Microsoft.EntityFrameworkCore.ServerVersion.Parse("11.8.3-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
