bfa8044236211be71243bd242ad7cb661ee4d70f
Gepyy <gordya1564@gmail.com>
2026-04-19 22:21:39 +0300

    DefaultConnection was changed

--- Infrastructure/Database/AppDbContext.cs
+++ Infrastructure/Database/AppDbContext.cs
@@ -95,7 +95,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("host=127.0.0.1;port=3307;database=DNUProjectDb;username=user_dnupr;password=B25824DCABCB88B5", ServerVersion.Parse("11.8.3-mariadb"));
+        => optionsBuilder.UseMySql("host=127.0.0.1;port=5432;database=project_olymp_db;username=postgres;password=B25824DCABCB88B5", ServerVersion.Parse("11.8.3-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
